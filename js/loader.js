ad58b7f0efe630c0a8db004d1bb3a5a44d1e7e83
colorodriguezf <colitorodriguez97@gmail.com>
2023-07-25 11:43:28 -0300

    solucionado bug loader

--- js/loader.js
+++ js/loader.js
@@ -3,11 +3,12 @@ const contenedorIndex = document.querySelector('.contenido');
 const loader = document.querySelector('.loader');
 const lmain = document.querySelector('.l-main');
 window.addEventListener("DOMContentLoaded", (event) => {
-    contenedorIndex.style.display = "none";
-    lmain.style.display = "none";
+     lmain.style.display = "none"; 
     setTimeout(() => {
+    contenedorIndex.classList.remove("contenido");
+    contenedorIndex.classList.add("contenido__visible");
      loader.style.display = 'none';
-     contenedorIndex.style.display = "block";
+    //  contenedorIndex.style.display = "block";
      lmain.style.display = "block";    
   }, 8000);
   });
