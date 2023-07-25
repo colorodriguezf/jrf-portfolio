a897ad8bad3d54feb4e0ae5dc3a6b77b6bf3a2e1
colorodriguezf <colitorodriguez97@gmail.com>
2023-07-25 11:12:44 -0300

    Se agrego loader

--- js/loader.js
+++ js/loader.js
@@ -1,9 +1,10 @@
 "use strict"
-const contenedorIndex = document.getElementById("contenedorIndex");
-const loader = document.getElementById("loader");
-document.addEventListener("DOMContentLoaded", function(event) {
-    // setTimeout(() => {
-    //     loader.style.display = 'none';
-        
-    // }, 3000);
-});
+const contenedorIndex = document.querySelector('.contenido');
+const loader = document.querySelector('.loader');
+window.addEventListener("DOMContentLoaded", (event) => {
+    contenedorIndex.style.display = "none";
+    setTimeout(() => {
+     loader.style.display = 'none';
+     contenedorIndex.style.display = "block";  
+  }, 7000);
+  });
