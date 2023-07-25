9fec89771f0ecab2d3890251f209e21d95b4d6e4
colorodriguezf <colitorodriguez97@gmail.com>
2023-07-25 11:20:51 -0300

    Modifico loader

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,13 @@
 "use strict"
 const contenedorIndex = document.querySelector('.contenido');
 const loader = document.querySelector('.loader');
+const lmain = document.querySelector('.l-main');
 window.addEventListener("DOMContentLoaded", (event) => {
     contenedorIndex.style.display = "none";
+    lmain.style.display = "none";
     setTimeout(() => {
      loader.style.display = 'none';
-     contenedorIndex.style.display = "block";  
-  }, 7000);
+     contenedorIndex.style.display = "block";
+     lmain.style.display = "block";    
+  }, 8000);
   });
