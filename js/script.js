db0ba49273816310470dbf685ad21850ec318086
colorodriguezf <colitorodriguez97@gmail.com>
2023-08-24 11:58:30 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -268,3 +268,9 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+
+
+const checkbox = document.getElementById("checkbox");
+checkbox.addEventListener("change", () => {
+  
+})
