e589a591e7b602fc7a23783e69bc952aa45e7911
colorodriguezf <colitorodriguez97@gmail.com>
2023-12-20 10:47:09 -0300

    problema de pasar de home a work o de about a work, se para en about el nav

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ function scrollActive(){
   
   sections.forEach(current =>{
     let sectionHeight = current.offsetHeight
-    let sectionTop = current.offsetTop - 70;
+    let sectionTop = current.offsetTop - 50;
     sectionId = current.getAttribute('id')
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
